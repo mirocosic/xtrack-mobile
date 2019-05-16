7841eda315a2fe3b6c7ad46e47d8d2e771d2ee0a
Miro <mirocosic@gmail.com>
2019-05-16 09:55:07 +0200

    add transaction component

---
+++ src/components/add-transaction/component.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from "react-native";
+import palette from "../../utils/palette"
+
+export default StyleSheet.create({
+
+    accountCard: {
+      backgroundColor: "white",
+      margin: 20,
+      borderRadius: 10
+    },
+
+    accountCardDark: {
+      backgroundColor: palette.dark
+    },
+
+    accountDetails: {
+      paddingLeft: 20,
+    },
+
+    overview: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      padding: 20
+    },
+
+    addButton: {
+      position: "absolute",
+      bottom: 20,
+      marginRight: 20,
+      alignSelf: "flex-end",
+      right: 0,
+      alignItems: "center",
+      justifyContent: "center",
+      padding: 0,
+    },
+
+})
---
+++ src/components/add-transaction/index.js
@@ -0,0 +1,18 @@
+import { connect } from "react-redux";
+import Component from "./component";
+
+export default connect(
+  state => ({
+    accounts: state.accounts.items,
+    accountFilter: state.accounts.accountFilter,
+    entries: state.transactions.entries,
+    total: state.transactions.total,
+    expenses: state.transactions.expenses,
+    income: state.transactions.income,
+  }),
+
+  dispatch => ({
+    clearTransactionForm: () => dispatch({ type: "CLEAR_TRANSACTION_FORM" }),
+    clearSelectedCategory: () => dispatch({ type: "CLEAR_SELECTED_CATEGORY" }),
+  }),
+)(Component);
