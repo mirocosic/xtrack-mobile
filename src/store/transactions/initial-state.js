ca5c3dc721fb0e2feb9dec53a5dcce0af9710830
Miro <mirocosic@gmail.com>
2019-05-16 09:55:44 +0200

    new initial state

--- src/store/transactions/initial-state.js
+++ src/store/transactions/initial-state.js
@@ -1,11 +1,10 @@
 export default {
-  total: -300,
-  expenses: -300,
+  total: 0,
+  expenses: 0,
   income: 0,
   selectedTransaction: { labels: [], account: {}, fromAccount: {} },
   entries: [
-    { id: 0, timestamp: 0, type: "expense", amount: 100, category: { id:0, name: "Groceries" }, note: "Well spent!", labels: [], account: {} },
-    { id: 1, timestmp: 0, type: "expense", amount: 200, category: { id: 1, name: "Rent" }, note: "Well spent 2!", labels: [], account: {} },
+
   ],
   transferMode: false,
 
