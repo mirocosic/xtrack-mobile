df140d09507a8bc3d1352800bb9048cd7513b353
Miro <mirocosic@gmail.com>
2019-05-17 12:36:04 +0200

    added RN community AsyncStorage for future use

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,18 @@
-import { createStore, combineReducers } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+import { createStore, combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { AsyncStorage } from "@react-native-community/async-storage"
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 
 
-import common from './common/reducer';
-import transactions from './transactions/reducer';
-import categories from './categories/reducer';
-import accounts from './accounts/reducer';
-import labels from './labels/reducer';
+import common from "./common/reducer";
+import transactions from "./transactions/reducer";
+import categories from "./categories/reducer";
+import accounts from "./accounts/reducer";
+import labels from "./labels/reducer";
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
   stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
 };
