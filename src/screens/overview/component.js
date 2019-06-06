e7255ac5b6bba1e4a4f2ec52aa9c9e996d7d9c09
Miro <mirocosic@gmail.com>
2019-06-06 11:25:48 +0200

    overview cmp update

--- src/screens/overview/component.js
+++ src/screens/overview/component.js
@@ -4,42 +4,45 @@ import { View, Text } from "react-native"
 import { withNavigation } from "react-navigation"
 import Screen from "../../components/screen"
 import Header from "../../components/header"
-
+import Icon from "../../components/icon"
 import AddTransaction from "../../components/add-transaction"
 import styles from "./styles"
 
 
 class Overview extends Component {
 
+  static navigationOptions = () => ({
+    tabBarIcon: ({ tintColor }) => (
+      <Icon style={{ backgroundColor: "white" }} textStyle={{ fontSize: 26, color: tintColor }} type="tachometer-alt" />
+    ),
+  })
+
   state = {}
 
   renderExpenses() {
     return Object.entries(this.props.expensesByCategory).map(item => (
       <View style={{ ...styles.row, paddingLeft: 20 }}>
-        <Text>{item[0]}</Text>
-        <Text>{item[1]}</Text>
+        <Text>{`${item[0]} `}</Text>
+        <Text>{`${item[1]} kn`}</Text>
       </View>
     ))
   }
 
   render() {
-
-    const { expensesByCategory } = this.props
-
-    console.log(expensesByCategory)
+    const { expensesByCategory, income, expenses } = this.props
 
     return (
       <Screen>
         <Header title="Overview" />
         <View style={styles.wrap}>
           <View style={styles.row}>
-            <Text>Income</Text>
-            <Text>1000 kn</Text>
+            <Text>Income </Text>
+            <Text>{income} kn</Text>
           </View>
 
           <View style={styles.row}>
-            <Text>Expenses</Text>
-            <Text>500 kn</Text>
+            <Text>Expenses </Text>
+            <Text>{expenses} kn</Text>
           </View>
 
           <View style={styles.breakdownWrap}>
@@ -47,8 +50,8 @@ class Overview extends Component {
           </View>
 
           <View style={styles.row}>
-            <Text>Balance</Text>
-            <Text>10000</Text>
+            <Text>Balance </Text>
+            <Text>{income - expenses} kn</Text>
           </View>
 
 
