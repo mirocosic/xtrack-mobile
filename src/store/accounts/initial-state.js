cc6e38395e764dbeb43b97b410c09627f2a090b6
Miro <mirocosic@gmail.com>
2021-11-16 22:19:51 +0100

    added initial category and account

--- src/store/accounts/initial-state.js
+++ src/store/accounts/initial-state.js
@@ -1,5 +1,5 @@
 export default {
-  items: [],
+  items: [{id: "c4526cb8-f0eb-4af6-ac09-b0b1f504ea85", name: "Main", icon: "creditCard", color: "#2196F3", startingBalance: 0, currency: "HRK", defaultAccount: true}],
   toAccount: { id: 0, name: "Savings" },
   fromAccount: {},
   accountFilter: false,
--- src/store/categories/initial-state.js
+++ src/store/categories/initial-state.js
@@ -1,5 +1,5 @@
 export default {
-  items: [],
+  items: [{id: "da42d4ba-b9ba-4d84-a82b-b029f15f1733", name: "General", icon: "shoppingBasket", color: "#0097A7", defaultCategory: true}],
   selectedCategory: {},
   categoryFilter: false,
 }
