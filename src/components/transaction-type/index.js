a0a5bed167801465a32114b8c7c82be33e4b8df9
Miro Cosic <mirocosic@gmail.com>
2021-12-11 15:53:58 +0100

    transaction type fixed and rewritten to fn comp

--- src/components/transaction-type/index.js
+++ src/components/transaction-type/index.js
@@ -1,10 +1,6 @@
-import React, { Component } from "react"
+import React, { useEffect, useState, useRef } from "react"
 import { Animated, Easing, View, TouchableOpacity } from "react-native"
-import {
-  FlingGestureHandler,
-  Directions,
-  State,
-} from "react-native-gesture-handler"
+import { FlingGestureHandler, Directions, State } from "react-native-gesture-handler"
 import ReactNativeHapticFeedback from "react-native-haptic-feedback"
 import { Copy } from "../typography"
 import __ from "../../utils/translations"
@@ -12,136 +8,104 @@ import styles from "./styles"
 
 const offsets = { expense: 0, income: 120, transfer: 240 }
 
-class TransactionType extends Component {
-  state = {
-    currentType: this.props.transaction.type,
-    xOffset: new Animated.Value(offsets[this.props.transaction.type] || 0),
-  }
+const TransactionType = (props) => {
+  const { setType, transaction: { type }} = props
+  const offsetX = useRef(new Animated.Value(0)).current
 
-  touchX = new Animated.Value(0)
-
-  onPanGestureEvent = Animated.event([{ nativeEvent: { x: this.touchX } }], {
-    useNativeDriver: true,
+  const color = offsetX.interpolate({
+    inputRange: [0, 100, 200],
+    outputRange: [
+      "rgba(189, 13, 13, 1)",
+      "rgba(3, 120, 1, 1)",
+      "rgba(4, 80, 135, 1)",
+    ],
   })
 
-  onClick = type => {
-    const { setType } = this.props
-    const { xOffset } = this.state
-    let position = 0
+  const width = offsetX.interpolate({
+    inputRange: [0, 60, 120, 180, 240],
+    outputRange: [100, 160, 100, 160, 100],
+  })
 
-    switch (type) {
-      case "expense":
-        position = 0
-        setType("expense")
-        this.setState({ currentType: "expense" })
-        break
-      case "income":
-        position = 120
-        setType("income")
-        this.setState({ currentType: "income" })
-        break
-      case "transfer":
-        position = 240
-        setType("transfer")
-        this.setState({ currentType: "transfer" })
-        break
-      default:
-        position = 0
-    }
-    Animated.timing(xOffset, {
-      toValue: position,
-      easing: Easing.bezier(0.645, 0.045, 0.355, 1),
-      duration: 450,
-      useNativeDriver: false
-    }).start()
-  }
+  useEffect(
+    () => {
+      Animated.timing(offsetX, {
+        toValue: offsets[type],
+        easing: Easing.bezier(0.645, 0.045, 0.355, 1),
+        duration: 450,
+        useNativeDriver: false
+      }).start()
 
-  render() {
-    const { xOffset, currentType } = this.state
+    }, [type]
+  )
 
-    const color = xOffset.interpolate({
-      inputRange: [0, 100, 200],
-      outputRange: [
-        "rgba(189, 13, 13, 1)",
-        "rgba(3, 120, 1, 1)",
-        "rgba(4, 80, 135, 1)",
-      ],
-    })
+  return (
+    <View style={{ alignItems: "center", padding: 10 }}>
+      <FlingGestureHandler
+        direction={Directions.RIGHT}
+        onHandlerStateChange={({ nativeEvent }) => {
+          if (nativeEvent.state === State.ACTIVE) {
+            if (type === "expense") { setType("income") }
+            if (type === "income") { setType("transfer")}
+          }
 
-    const width = xOffset.interpolate({
-      inputRange: [0, 60, 120, 180, 240],
-      outputRange: [100, 160, 100, 160, 100],
-    })
-
-    return (
-      <View style={{ alignItems: "center", padding: 10 }}>
+          if (nativeEvent.state === State.END) {
+            ReactNativeHapticFeedback.trigger("impactLight")
+          }
+        }}
+      >
         <FlingGestureHandler
-          direction={Directions.RIGHT}
+          direction={Directions.LEFT}
           onHandlerStateChange={({ nativeEvent }) => {
             if (nativeEvent.state === State.ACTIVE) {
-              if (currentType === "expense") { this.onClick("income")}
-              if (currentType === "income") { this.onClick("transfer")}
+              if (type === "transfer") { setType("income")}
+              if (type === "income") { setType("expense")}
             }
 
-            if (nativeEvent.state === State.END) {
-              ReactNativeHapticFeedback.trigger("impactLight")
-            }
+            ReactNativeHapticFeedback.trigger("impactLight")
           }}
         >
-          <FlingGestureHandler
-            direction={Directions.LEFT}
-            onHandlerStateChange={({ nativeEvent }) => {
-              if (nativeEvent.state === State.ACTIVE) {
-                if (currentType === "transfer") { this.onClick("income")}
-                if (currentType === "income") { this.onClick("expense")}
-              }
+          <Animated.View style={styles.formFieldWrap}>
+            <Animated.View
+              style={[
+                styles.slider,
+                {
+                  backgroundColor: color,
+                  height: 40,
+                  width,
+                  left: offsetX,
+                },
+              ]}
+            />
 
-              ReactNativeHapticFeedback.trigger("impactLight")
-            }}
-          >
-            <Animated.View style={styles.formFieldWrap}>
-              <Animated.View
-                style={[
-                  styles.slider,
-                  {
-                    backgroundColor: color,
-                    height: 40,
-                    width,
-                    left: xOffset,
-                  },
-                ]}
-              />
+            <TouchableOpacity
+              onPress={() => setType("expense")}
+              style={[styles.transactionTypeButton]}>
+              <View style={styles.typeWrap}>
+                <Copy style={{ color: "white" }}>EXPENSE</Copy>
+              </View>
+            </TouchableOpacity>
 
-              <TouchableOpacity
-                onPress={() => this.onClick("expense")}
-                style={[styles.transactionTypeButton]}>
-                <View style={styles.typeWrap}>
-                  <Copy style={{ color: "white" }}>EXPENSE</Copy>
-                </View>
-              </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => setType("income")}
+              style={[styles.transactionTypeButton]}>
+              <View style={styles.typeWrap}>
+                <Copy style={{ color: "white" }}>INCOME</Copy>
+              </View>
+            </TouchableOpacity>
 
-              <TouchableOpacity
-                onPress={() => this.onClick("income")}
-                style={[styles.transactionTypeButton]}>
-                <View style={styles.typeWrap}>
-                  <Copy style={{ color: "white" }}>INCOME</Copy>
-                </View>
-              </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => setType("transfer")}
+              style={[styles.transactionTypeButton]}>
+              <View style={styles.typeWrap}>
+                <Copy style={{ color: "white" }}>TRANSFER</Copy>
+              </View>
+            </TouchableOpacity>
 
-              <TouchableOpacity
-                onPress={() => this.onClick("transfer")}
-                style={[styles.transactionTypeButton]}>
-                <View style={styles.typeWrap}>
-                  <Copy style={{ color: "white" }}>TRANSFER</Copy>
-                </View>
-              </TouchableOpacity>
-
-            </Animated.View>
-          </FlingGestureHandler>
+          </Animated.View>
         </FlingGestureHandler>
-      </View>
-    )
-  }
+      </FlingGestureHandler>
+    </View>
+  )
 }
 
 export default TransactionType
